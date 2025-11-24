214736d006a634b37983a617b7beafd929f78f68
Kastriot <kastriot@Kastriots-Mac-mini.local>
2025-11-24 16:28:43 +0100

    Add newsletter signup, custom 44 page, and blog UX improvements

--- polepositionautomation/settings.py
+++ polepositionautomation/settings.py
@@ -31,6 +31,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -176,3 +177,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
